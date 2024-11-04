46c6fd325d42a9b6328387b4b6f8d087132d6858
angelettecortez <cortezangelette@gmail.com>
2024-11-04 18:16:35 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Angelette H. Cortez";
+let age = 19;
+let favoriteNumber = 12;
+let favoriteColor = "yellow";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
